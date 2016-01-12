9d765467e21850c9c34ef879ae051ce938aa2f35
Nick Webster <nick@nick.geek.nz>
2016-01-12 20:22:36 +1300

    I got something working

--- pastry.py
+++ pastry.py
@@ -30,14 +30,17 @@ ANY = '0.0.0.0'
 MCAST_ADDR = '224.1.1.1'
 MCAST_PORT = 2679
 listenSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+listenSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listenSock.bind(('', MCAST_PORT))
+listenSock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 255)
+listenSock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton(MCAST_ADDR)+socket.inet_aton(ANY))
 
 #Send out the new clipboard
 def sendClipboard(clipboard):
-	global listenSock
 	#Hook up the multicast
-	# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-	listenSock.bind((ANY, MCAST_PORT))
-	listenSock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+	sock.bind(('', 2678))
+	sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
 
 	#Send the clipboard
 	try:
@@ -45,27 +48,21 @@ def sendClipboard(clipboard):
 			clipboard = clipboard.clipboard.decode('utf-8')
 		copiedText = json.dumps({"id": settings.login['id'], "data": clipboard}).encode("utf-8")
 		copiedText = zlib.compress(copiedText, 9)
-		listenSock.sendto(copiedText, (MCAST_ADDR,MCAST_PORT))
+		sock.sendto(copiedText, (MCAST_ADDR,MCAST_PORT))
 	except Exception as e:
 		eg.msgbox("There was an error sending your clipboard.", "Pastry")
 		print("Error: %s" % str(e))
 
 	#End the connection
-	# listenSock.close()
+	sock.close()
 
 def listen():
 	global currentClipboard
 	global listenSock
 	while 1:
 		time.sleep(0.1)
-		#Check for external messages
-		listenSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		listenSock.bind((ANY,MCAST_PORT))
-		listenSock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 255)
-		status = listenSock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton(MCAST_ADDR)+socket.inet_aton(ANY))
-
 		try:
-			data, addr = listenSock.recvfrom(10240)
+			data, addr = listenSock.recv()
 		except socket.error:
 			pass
 		else:
